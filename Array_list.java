69ce97307d102d760992e551f0a9a29957359380
Pallavi <shettypallavi872@gmail.com>
2025-05-16 21:34:39 +0530

    Create Array_list.java

---
+++ Array_list.java
@@ -0,0 +1,16 @@
+/*1b.Array List programs*/
+
+/*1. Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not*/
+
+/*2. Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList*/
+
+/*4. Write a java program for getting different colors through ArrayList interface and sort them
+using Collections.sort( ArrayListObj) */
+
+/*5. Write a java program for getting different colors through ArrayList interface and extract the
+elements 1st and 2nd from the ArrayList object by using SubList()*/
+
+6. Write a java program for getting different colors through ArrayList interface and delete nth
+element from the ArrayList object by using remove by index
